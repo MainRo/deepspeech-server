58237fdb4a4ab8896682dd388a3cfc0626c3b0ca
Romain Picard <romain.picard@softathome.com>
2017-12-05 15:58:40 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
 
 setup(
     name="deepspeech-server",
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/MainRo/deepspeech-server.git',
     license='MPL-2.0',
     description="server for mozilla deepspeech",
